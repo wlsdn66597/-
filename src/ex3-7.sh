36632e5cab4fc2ed5e9bed6e677742f34ff9ae5c
wlsdn66597 <rlawlsdn0119@gmail.com>
2022-11-04 21:04:27 +0900

    과제#3 수정

--- src/ex3-7.sh
+++ src/ex3-7.sh
@@ -6,7 +6,14 @@ eval touch file0.txt
 eval touch file1.txt
 eval touch file2.txt
 eval touch file3.txt
-eval touch file4.txt
+eval touch filej4.txt
 eval mkdir -p file0
 eval ln -s file0.txt file0
-
+eval mkdir -p file1
+eval ln -s file1.txt file1
+eval mkdir -p file2
+eval ln -s file2.txt file2
+eval mkdir -p file3
+eval ln -s file3.txt file3
+eval mkdir -p file4
+eval ln -s file4.txt file4
